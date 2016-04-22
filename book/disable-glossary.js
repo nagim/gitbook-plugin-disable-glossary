ddb29ae5aec020f63ad881e1eddfcac9cd3217ae
nagim <imi.nagy1@gmail.com>
2016-04-22 12:56:09 +0200

    Added elements

--- book/disable-glossary.js
+++ book/disable-glossary.js
@@ -1,6 +1,6 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("page.change", function() {
-        $('pre .glossary-term')
+        $('h1, h2, em, a .glossary-term')
           .removeAttr('href')
           .removeAttr('title')
           .removeClass('glossary-term')
